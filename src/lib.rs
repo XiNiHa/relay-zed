04ae764cccb91314b6c70fb0524891d7f05a1a3c
Iha Shin <me@xiniha.dev>
2025-04-24 19:12:44 +0900

    rename pathToBinary to pathToRelay

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ impl RelayZed {
         path_to_relay: Option<String>,
     ) -> zed::Result<String> {
         if let Some(path) = path_to_relay {
-            println!("You've manually specified 'pathToBinary'. We cannot confirm this version of the Relay Compiler is supported by this version of the extension. I hope you know what you're doing.");
+            println!("You've manually specified 'pathToRelay'. We cannot confirm this version of the Relay Compiler is supported by this version of the extension. I hope you know what you're doing.");
             return Ok(path);
         }
 
@@ -139,7 +139,7 @@ impl Settings {
                     .map(|v| v.to_string())
             }),
             path_to_relay: settings.settings.as_ref().and_then(|s| {
-                s.get("pathToBinary")
+                s.get("pathToRelay")
                     .and_then(|v| v.as_str())
                     .map(|v| v.to_string())
             }),
